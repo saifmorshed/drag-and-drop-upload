c889cd629aa23213d420e705a1fe5e1161424e52
saifmorshed <saifpt2@gmail.com>
2016-02-07 05:00:35 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+ 
+<head>
+<title> Php Drag and drop uploading </title>   
+ <!-- link with dropzone css -->
+<link href="css/dropzone.css" type="text/css" rel="stylesheet" />
+ <!-- link with dropzone js -->
+<script src="js/dropzone.js"></script>
+ 
+</head>
+ 
+<body>
+ 
+    <div style="width:70%;margin:auto; margin-top:50px">
+        <!-- Add simple code here -->
+        <form action="upload.php" class="dropzone"></form>
+        
+    </div>  
+</body>
+ 
+</html>
